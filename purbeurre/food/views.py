d3b8f2a5a8f5ac50889e6c2f2d9d8d332510771c
CaroleSa <carole220287@hotmail.com>
2019-10-14 14:30:30 +0200

    add new views in food

--- purbeurre/food/views.py
+++ purbeurre/food/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'food/index.html')
+
+def result(request):
+    return render(request, 'food/result.html')
+
+def detail(request):
+    return render(request, 'food/detail.html')
+
+def favorites(request):
+    return render(request, 'food/favorites.html')
