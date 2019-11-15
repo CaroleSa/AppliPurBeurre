810f0936d555bace3b14fc6ab29358e3e33e2bb5
CaroleSa <carole220287@hotmail.com>
2019-11-15 10:45:29 +0100

    view account appli ok 0

--- purbeurre/account/views.py
+++ purbeurre/account/views.py
@@ -5,93 +5,46 @@
 
 
 # Imports
-from django.shortcuts import render, redirect
-import re
+from django.shortcuts import render
 from account.forms import Account
-
-import hashlib
 from django.contrib.auth import get_user_model
-
-
-
+from django.contrib.auth import authenticate, login
 
 
 
 def access_account(request):
     form = Account()
     context = {'form': form}
-    user = get_user_model()
 
     if request.method == 'POST':
         form = Account(request.POST)
+        # get the user's data
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # verify that the user exists
+        user = authenticate(email=email, password=password)
 
-        error_message = str(form.errors.as_data()['email'][0])
-        email_exists = "['Un objet Utilisateur avec ce champ Adresse électronique existe déjà.']"
+        # if user exists - connection and confirmation message
+        if user:
+           login(request, user)
+           context["message"] = ["Vous êtes connecté."]
+           context["color"] = "green"
 
-        # if the data entered by the user is valid
-        if form.is_valid() is True or error_message == email_exists:
-            print(error_message, "marcheLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
-
-            # if e-mail is valid
-            email = str(request.POST.get('e_mail'))
-            regexp = r"(^[a-z0-9._-]+@[a-z0-9._-]+\.[(com|fr)]+)"
-            if re.match(regexp, email) is not None:
-
-                # if user's e-mail exists to the database
-                data = user.objects.values_list('e_mail')
-                for elt in data:
-                    elt = elt[0]
-                    if email == elt:
-                        # encrypted user's password
-                        password = request.POST.get('password')
-                        password = password.encode()
-                        encrypted_password = hashlib.sha1(password).hexdigest()
-                        # get password encrypted to the databse
-                        data = user.objects.values_list('password')
-                        password_database = str(data.get(e_mail=email)[0])
-
-                        # if the user's password ok, return my_account page
-                        if password_database == encrypted_password:
-                            data = user.objects.values_list('creation_date')
-                            date = data.get(e_mail=email)[0]
-                            context = {'date': date, 'mail': email}
-                            return render(request, 'account/my_account.html', context)
-
-                        # if the user's password don't ok
-                        else:
-                            context["message"] = "Le compte existe mais le mot de passe n'est pas valide."
-                            context["color"] = "red"
-
-                    # if user's e-mail don't exists to the database
-                    else:
-                        context["message"] = "Ce compte n'existe pas."
-                        context["color"] = "red"
-
-            # if e-mail isn't valid
-            else:
-                if len(email) >= 6:
-                    context["message"] = "L'e-mail n'est pas valide."
+        elif not user:
+            if form.is_valid() is False:
+                # add the error messages in the context dictionary
+                error_list = []
+                for key, value in form.errors.as_data().items():
+                    message = str(key).title() + ' : ' + str(value[0]).replace("['", "").replace("']", "")
+                    error_list.append(message)
+                    context["message"] = error_list
                     context["color"] = "red"
-
-        # if the data entered by the user is not valid
-        else:
-            # if e-mail is not valid
-            email = str(request.POST.get('e_mail'))
-            regexp = r"(^[a-z0-9._-]+@[a-z0-9._-]+\.[(com|fr)]+)"
-            if re.match(regexp, email) is None:
-                context["message"] = "L'e-mail n'est pas valide."
+            else:
+                # add the error message in the context dictionary
+                context["message"] = ["Ce compte n'existe pas."]
                 context["color"] = "red"
 
-            # if e-mail is valid
-            else:
-                password = request.POST.get('password')
-                # if user's password is too long
-                if len(password) > 8:
-                    context["message"] = "Votre mot de passe doit contenir 8 caractères"
-                    context["color"] = "red"
-
-    return render(request, 'account/access_account.html', context)
-
+        return render(request, 'account/access_account.html', context)
 
 
 def create_account(request):
@@ -108,13 +61,11 @@ def create_account(request):
 
         # if the data entered by the user is valid
         if form.is_valid() is True:
-
             # if password and password control are the same
             if password_control == password:
                 user.objects.create_user(username='Null', email=email, password=password)
                 context["message"] = ["Votre compte a bien été créé."]
                 context["color"] = "green"
-
             # if password and password control aren't the same
             else:
                 # create error message
