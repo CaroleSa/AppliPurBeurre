d43c0b4f7866435eaf9b9ec2aeb84098d1c92753
CaroleSa <carole220287@hotmail.com>
2019-11-21 16:33:13 +0100

    test call-api ok 0

--- purbeurre/food/classes/test_call_api.py
+++ purbeurre/food/classes/test_call_api.py
@@ -6,7 +6,7 @@
 # import
 from unittest import TestCase
 from unittest.mock import patch
-from purbeurre.food.classes.call_api import CallApi
+from food.classes.call_api import CallApi
 
 
 
@@ -16,17 +16,17 @@ class TestCallApi(TestCase):
     def setUp(self):
         self.new_call_api = CallApi()
 
-    @patch('purbeurre.food.classes.call_api.requests.get')
+    @patch('food.classes.call_api.requests.get')
     def test_return_request(self, mock_api):
 
-        result_json = [{'page': '1', 'count': 0, 'skip': 0, 'page_size': '100', 'products': []},
-                       {'page': '1', 'count': 0, 'skip': 0, 'page_size': '100', 'products': []},
-                       {'products': [], 'page_size': '100', 'skip': 0, 'count': 0, 'page': '1'},
-                       {'page': '1', 'count': 0, 'skip': 0, 'page_size': '100', 'products': []},
-                       {'skip': 0, 'count': 8, 'page': '1', 'products': ['data'], 'page_size': '100'}]
-
+        result_json = {'count': 0, 'page_size': '100', 'products': [], 'page': '1', 'skip': 0}, \
+                      {'skip': 0, 'page': 1, 'products': [], 'page_size': 100, 'count': 0}, \
+                      {'skip': 0, 'page': '1', 'products': [], 'page_size': '100', 'count': 0}, \
+                      {'page': '1', 'skip': 0, 'count': 0, 'page_size': '100', 'products': []}, \
+                      {'skip': 0, 'page': '1', 'page_size': '100', 'products':[], 'count': 10}
         mock_api.return_value.json.return_value = result_json
+        result_list = [result_json]
 
         categories_list = ["pizza"]
 
-        self.assertEqual(self.new_call_api.load_data(categories_list), result_json)
+        self.assertEqual(self.new_call_api.load_data(categories_list), result_list)
