f30c71db0545601ca6675583f4f65198f14d2329
CaroleSa <carole220287@hotmail.com>
2019-11-26 12:50:36 +0100

    context_processor.py clean pep8 ok

--- purbeurre/food/context_processor.py
+++ purbeurre/food/context_processor.py
@@ -1,7 +1,17 @@
+#! /usr/bin/env python3
+# coding: UTF-8
+
+""" Context processor """
+
+
+# import
 from django.conf import settings
 
+
 def menu(request):
+    """ add the authenticated status
+    in all the contexts """
     if request.user.is_authenticated:
         return {'authenticated': 'True'}
-    else:
-        return {'authenticated': 'False'}
+
+    return {'authenticated': 'False'}
