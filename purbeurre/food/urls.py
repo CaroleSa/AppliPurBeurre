14e0df9617315b845f4778bd87ae15e9ea544966
CaroleSa <carole220287@hotmail.com>
2019-11-26 12:57:17 +0100

    urls.py in food appli clean pep8 ok

--- purbeurre/food/urls.py
+++ purbeurre/food/urls.py
@@ -1,3 +1,10 @@
+#! /usr/bin/env python3
+# coding: UTF-8
+
+""" Food URLS """
+
+
+# imports
 from django.conf.urls import url
 from . import views
 
@@ -5,7 +12,7 @@ from . import views
 app_name = 'food'
 
 urlpatterns = [
-    url(r'^result/$', views.result,  name="result"),
+    url(r'^result/$', views.result, name="result"),
     url(r'^detail/$', views.detail, name="detail"),
     url(r'^favorites/$', views.favorites, name="favorites")
 ]
