39e44c044a3eb87b1b46727c5033f5e2e7e90074
CaroleSa <carole220287@hotmail.com>
2019-11-26 10:37:53 +0100

    views.py account appli clean ok

--- purbeurre/account/views.py
+++ purbeurre/account/views.py
@@ -1,44 +1,49 @@
 #! /usr/bin/env python3
 # coding: UTF-8
 
-""" Views """
+""" Account views :
+access_account
+my_account
+create_account """
 
 
 # Imports
 from django.shortcuts import render
-from account.forms import Account
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login
 import re
-
+from account.forms import Account
 
 
 def access_account(request):
+    """ access_account view :
+    connects the user if the information given is correct
+    else, display an error message """
+
+    # add Account form in the context
     form = Account()
     context = {'form': form}
 
     if request.method == 'POST':
+        # GET THE USER'S DATA AND VERIFY THAT THE ACCOUNT EXISTS
         form = Account(request.POST)
-        # get the user's data
         email = request.POST.get('email')
         password = request.POST.get('password')
-        # verify that the user exists
         user = authenticate(email=email, password=password)
 
-        # USER'S CONNECTION AND DISPLAY THE INDEX PAGE
+        # USER'S CONNECTION AND
+        # DISPLAY A CONFIRMATION MESSAGE ON THE INDEX PAGE
         # if user exists
         if user:
-            # user's connection
             login(request, user)
             mail = request.user.email
             context = {'message': "Bonjour {} ! Vous êtes bien connecté.".format(mail)}
             return render(request, 'food/index.html', context)
 
-        # DISPLAY AN ERROR MESSAGE
+        # DISPLAY AN ERROR MESSAGE ON THE ACCESS_ACCOUNT PAGE
         # if user does not exists
         elif not user:
             if form.is_valid() is False:
-                # add the error messages in the context dictionary
                 error_list = []
                 for key, value in form.errors.as_data().items():
                     message = str(key).title() + ' : ' + str(value[0]).replace("['", "").replace("']", "")
@@ -46,7 +51,6 @@ def access_account(request):
                     context["message"] = error_list
                     context["color"] = "red"
             else:
-                # add the error message in the context dictionary
                 context["message"] = ["Ce compte n'existe pas."]
                 context["color"] = "red"
             return render(request, 'account/access_account.html', context)
@@ -55,7 +59,9 @@ def access_account(request):
 
 
 def my_account(request):
-    # get the user's data
+    """ my_account view :
+    display the user's data on the my_account page """
+
     mail = request.user.email
     date = request.user.date_joined
     context = {'date': date, 'mail': mail}
@@ -63,39 +69,50 @@ def my_account(request):
 
 
 def create_account(request):
+    """ create_account view :
+        created the user's account if the information given is correct
+        else, display an error message on the create_account page """
+
+    # add Account form in the context
     user = get_user_model()
     form = Account()
     context = {'form': form}
 
     if request.method == 'POST':
+        # GET THE USER'S DATA
         form = Account(request.POST)
-        # get user's data
         email = request.POST.get('email')
         password_control = request.POST.get('passwordControl')
         password = request.POST.get('password')
 
-        # if the data entered by the user is valid
         if form.is_valid() is True:
+            # DISPLAY AN ERROR MESSAGE ON THE CREATE_ACCOUNT PAGE
+            # if the email isn't valid
             regex = r"^[a-z0-9-_.]+@[a-z0-9-]+\.(com|fr)$"
             result = re.match(regex, email)
             if result is None:
                 # create error message
                 context["message"] = ["Votre adresse e-mail n'est pas valide."]
                 context["color"] = "red"
+
+            # CREATE THE USER'S ACCOUNT AND
+            # DISPLAY A CONFIRMATION MESSAGE ON THE INDEX PAGE
+            # if the data entered by the user is valid
             else:
-                # if password and password control are the same
                 if password_control == password:
                     user.objects.create_user(username='Null', email=email, password=password)
                     context = {"message": "Le compte {} a bien été créé.".format(email)}
                     return render(request, 'food/index.html', context)
+
+                # DISPLAY AN ERROR MESSAGE ON THE CREATE_ACCOUNT PAGE
                 # if password and password control aren't the same
                 else:
-                    # create error message
                     context["message"] = ["Vos mots de passe ne sont pas identiques."]
                     context["color"] = "red"
 
+        # DISPLAY AN ERROR MESSAGE ON THE CREATE_ACCOUNT PAGE
+        # if the form isn't valid
         else:
-            # add the error messages in the context dictionary
             error_list = []
             for key, value in form.errors.as_data().items():
                 message = str(key).title() + ' : ' + str(value[0]).replace("['", "").replace("']", "")
