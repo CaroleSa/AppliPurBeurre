aadc2b05accf7ce9d7cdb0799c934591d2f5bb75
CaroleSa <carole220287@hotmail.com>
2019-11-28 10:56:27 +0100

    add line 163 settings.py 0

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 
 import os
+import dj_database_url
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -157,3 +159,6 @@ if os.environ.get('ENV') == 'PRODUCTION':
     )
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
